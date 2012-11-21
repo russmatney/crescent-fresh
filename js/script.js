0cff34885320dcaf1e03dc7e69722ff689a33b92
Russell Matney <russell.matney@gmail.com>
2012-11-21 02:30:18 -0500

    Added first functional files

---
+++ js/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+	$('#code ul li').hide();
+
+	$('#hide500').click(function(){
+		$('#block').hide(500);
+
+	});
+
+	$('#show500').click(function(){
+		$('#block').show(500);
+	});
+
+	// $('#RevFromBot').click(function(){
+	// 	$('#block').hide();
+	// 	$('#block').css({'height':'0','float':'left'});
+	// 	$('#block').show();
+	// 	$('#block').animate({'height':'80px'}, 500);
+	// });
+
+	$('#RevFromTop').click(function(){
+		$('#block').hide();
+		$('#block').css({'height':'0','float':'left'});
+		$('#block').show();
+		$('#block').animate({'height':'80px'}, 500);
+
+	});
+
+	$('#RevFromLeft').click(function(){
+		$('#block').hide();
+		$('#block').css({'width':'0','float':'left'});
+		$('#block').show();
+		$('#block').animate({'width':'300px'}, 500);
+	});
+
+	$('#RevFromRight').click(function(){
+
+		$.snow();
+
+		$('#block').hide();
+		$('#block').css({'width':'0','float':'right'});
+		$('#block').show();
+		$('#block').animate({'width':'300px'}, 500);
+
+	});
+
+
+
+	$('#snow').click(function(){
+		
+    	$.snow({
+    		flake_number: 100,
+            flake_folder: 'js/big-24/',
+            flake_imgs: 5,
+            linked_flakes: 0,
+            link: '',
+            melt: 450,
+            wind: 70,
+            rotation: 5,
+            speed: 10
+        });
+  	
+  	});
+
+	$('#demoButtons button').click(function(){
+		showCode(this);
+	});
+
+
+});
+
+var showCode = function(e){
+	var a = $(e).attr('id');
+	var b = '' + a + 'Code';
+	$('#code ul li').hide();
+	$('#'+b+'').show();	
+};
